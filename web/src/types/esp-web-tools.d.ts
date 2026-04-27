dd5d39a53c0656090900cabb3727a89bd1239355
engmung <122682380+engmung@users.noreply.github.com>
2026-04-27 18:59:21 +0900

    fix: Add TypeScript declarations for ESP web tools custom element

---
+++ web/src/types/esp-web-tools.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'esp-web-install-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        manifest?: string;
+      }, HTMLElement>;
+    }
+  }
+}
